e4a3ea8fa601a5aa0a252b938f2ec4256aab7599
dionlan <dionlan.alves@gmail.com>
2025-10-18 12:25:16 -0300

    Corrige output directory para vercel

--- next.config.js
+++ next.config.js
@@ -3,21 +3,7 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
